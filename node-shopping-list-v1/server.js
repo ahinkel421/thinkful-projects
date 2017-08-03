0026351807c69d9baa4cd31183649d3e063bac21
Adam Hinkel <ahinkel421@gmail.com>
2017-08-03 12:31:43 -0400

    initial commit

---
+++ node-shopping-list-v1/server.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+// log the http layer
+app.use(morgan('common'));
+
+// we're going to add some items to ShoppingList
+// so there's some data to look at
+ShoppingList.create('beans', 2);
+ShoppingList.create('tomatoes', 3);
+ShoppingList.create('peppers', 4);
+
+// when the root of this router is called with GET, return
+// all current ShoppingList items
+app.get('/shopping-list', (req, res) => {
+  //below is our API endpoint. Keep this 'thin' or simple.
+  //bulk of our code should be in our model.
+  res.json(ShoppingList.get());
+});
+
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+
+app.get('/recipes', (req, res) => {
+	res.json(Recipes.get());
+})
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
